28e7321aba2db31ef29969ecdc97eb5380dc5c28
Fu Diwei <fudiwei@sina.com>
2026-06-15 00:19:13 +0800

    add gitlab.ecloud.com/ecloud/ecloudsdkcore@v1.0.6

---
+++ gitlab.ecloud.com/ecloud/ecloudsdkcore@v1.0.6/go.mod
@@ -0,0 +1,5 @@
+module gitlab.ecloud.com/ecloud/ecloudsdkcore
+
+go 1.14
+
+require github.com/google/uuid v1.6.0
---
+++ gitlab.ecloud.com/ecloud/ecloudsdkcore@v1.0.6/region/region.go
@@ -0,0 +1,7 @@
+package region
+
+type Region struct {
+	PoolId   *string `json:"poolId"`
+	Endpoint *string `json:"endpoint"`
+	RegionId *string `json:"regionId"`
+}
